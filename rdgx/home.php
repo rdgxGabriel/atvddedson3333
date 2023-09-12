1698abcfa9b74f92aa54994f29eae531597f6861
Gabriel Rodrigues <128437303+rdgxGabriel@users.noreply.github.com>
2023-09-12 08:47:28 -0300

    Add files via upload

---
+++ rdgx/home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Controle de Despesas</title>
+    <link rel="stylesheet" href="css/estilos_menu.css">
+    <link rel="stylesheet" href="css/estilos.css"> <!-- Add a new CSS file for styling -->
+</head>
+<body>
+    <?php
+        require "menu.php"; 
+    ?>
+    <div class="container">
+        <h3>CONTROLE DE DESPESAS</h3>
+        <img src="imagens/img.jpg" alt="Controle de Despesas">
+    </div>
+</body>
+</html>
